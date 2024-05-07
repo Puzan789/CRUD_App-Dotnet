34d8f83019c1ec3ede105a95c02742d6d343fdc2
puzan <puzan936@gmail.com>
2024-05-07 17:07:44 +0545

    Bulkybook crud

---
+++ bulkybook/Bulkybookweb/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using Bulkybookweb.Models;
+using Microsoft.EntityFrameworkCore;
+namespace Bulkybookweb.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options ) :base(options)
+        {
+         
+        }
+        public DbSet<Category> Categories { get; set; }  //categories is called table name
+    }
+}
